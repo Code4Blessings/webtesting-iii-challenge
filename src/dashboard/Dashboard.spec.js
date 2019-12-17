5648b137c98f245422afc6afcec0f0e2448309d0
Code4Blessings <robinwarden@code4blessings.com>
2019-12-17 14:30:14 -0500

    Wrote tests for gates defaulting to open or closed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+import { testNameToKey } from "jest-snapshot/build/utils";
+
 // Test away
+
+test('Gate defaults to unlocked', () => {
+    const { getByText} = render(<Dashboard />)
+    getByText(/unlocked/i);
+})
+
+test('Gate defaults to open', () => {
+    const {getByText} = render(<Dashboard />)
+    getByText(/open/i);
+})
